05158b10783b56a28da89927cfad62c17e0ceec3
mattire <alr2d4@hotmail.com>
2026-01-25 23:20:31 +0200

    3.17 updateing number

--- index.js
+++ index.js
@@ -124,7 +124,18 @@ app.post('/api/persons', (request, response, next) => {
   }).catch(error => next(error));
 })
 
-
+app.put('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    }).catch(error => next(error));
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
